13f1f69f4ca076a9dd538c836f0f11e39fd29993
PrionetWeb <web@prionetproivder.network>
2026-04-29 18:38:04 -0700

    fix

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './'),
       '@/components': path.resolve(__dirname, './components'),
       '@/styles': path.resolve(__dirname, './styles'),
+      'figma:asset/dd91655823aa215811db66ecb73cccd7607f086a.png': path.resolve(__dirname, './assets/dd91655823aa215811db66ecb73cccd7607f086a.png'),
+      'figma:asset/afcde4263282c80b75d78e5ade359f73c1dbae80.png': path.resolve(__dirname, './assets/afcde4263282c80b75d78e5ade359f73c1dbae80.png'),
+      'figma:asset/37580bb4cfa59eb10ae033d4a76d46140e8629d6.png': path.resolve(__dirname, './assets/37580bb4cfa59eb10ae033d4a76d46140e8629d6.png'),
     },
   },
   server: {
